ceb781d42239c8462b4385579cee000439d49a6b
pashkes <pashkes@gmail.com>
2018-09-04 17:52:36 +0300

    sas

--- builds/static/js/maintxc89.js
+++ builds/static/js/maintxc89.js
@@ -2049,30 +2049,6 @@ object-assign
 })();
 
 (function () {
-  $('.js-slider-education').slick({
-    arrows: false,
-    dots: true,
-    mobileFirst: true,
-    adaptiveHeight: true,
-    responsive: [{
-      breakpoint: 767,
-      settings: {
-        variableWidth: true,
-        adaptiveHeight: false
-      }
-    },
-    {
-      breakpoint: 1343,
-      settings: {
-        variableWidth: false,
-        slidesToShow: 4
-      }
-    }]
-  });
-})();
-
-
-(function () {
   $('.js-departure-slider').slick({
     variableWidth: true,
     centerPadding: '0',
@@ -2095,6 +2071,30 @@ object-assign
     ]
   });
 })();
+
+(function () {
+  $('.js-slider-education').slick({
+    arrows: false,
+    dots: true,
+    mobileFirst: true,
+    adaptiveHeight: true,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        variableWidth: true,
+        adaptiveHeight: false
+      }
+    },
+    {
+      breakpoint: 1343,
+      settings: {
+        variableWidth: false,
+        slidesToShow: 4
+      }
+    }]
+  });
+})();
+
 
 
 
@@ -2335,15 +2335,11 @@ $('.js-select-lang-order').SumoSelect({
   captionFormat: '{0} Selected'
 });
 
-
-
 (function () {
   $('.js-menu-close').on('click', function () {
     $('body').removeClass('show-menu');
   });
 })();
-
-
 
 
 
@@ -2373,6 +2369,10 @@ $('.js-select-lang-order').SumoSelect({
     slickMobile(newsSlider, newsSliderSettigs);
   });
 })();
+
+
+
+
 
 
 
