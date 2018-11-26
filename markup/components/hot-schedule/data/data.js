07394319560540ace9208e92d73f4fd45ddcb3bd
pashkes <pashkes@gmail.com>
2018-11-26 18:20:01 +0200

    add page courses preparation

---
+++ markup/components/hot-schedule/data/data.js
@@ -0,0 +1,60 @@
+const data = {
+  closestCourses: {
+    rows: [
+      {
+        school: 'Кузьминки',
+        level: 'Pre-starter',
+        dayLessons: 'Вт, Чт',
+        timeLessons: '18:20&#8211;19:20',
+        hours: '3',
+        cost: '380—600 руб',
+        yearsOld: 'дети 7-8 лет'
+      },
+      {
+        school: 'Солнцево',
+        level: 'Разговорный',
+        dayLessons: 'Пн, Ср, Пт',
+        timeLessons: '18:20—19:20',
+        hours: '3',
+        cost: '380—600 руб',
+        yearsOld: 'Взрослые'
+      },
+      {
+        school: 'Выхино',
+        level: 'Pre-starter',
+        dayLessons: 'Вт, Чт',
+        timeLessons: '18:20&#8211;19:20',
+        hours: '3',
+        cost: '380—600 руб',
+        yearsOld: 'дети 7-8 лет'
+      },
+      {
+        school: 'Коломенская',
+        level: 'Разговорный',
+        dayLessons: 'Вт, Чт',
+        timeLessons: '18:20&#8211;19:20',
+        hours: '6',
+        cost: '380—600 руб',
+        yearsOld: 'Взрослые'
+      },
+      {
+        school: 'Хорвино',
+        level: 'Pre-starter',
+        dayLessons: 'Вт, Чт',
+        timeLessons: '18:20&#8211;19:20',
+        hours: '6',
+        cost: '380—600 руб',
+        yearsOld: 'дети 7-8 лет'
+      },
+      {
+        school: 'Выхино',
+        level: 'Разговорный',
+        dayLessons: 'Пн, Ср, Пт',
+        timeLessons: '18:20&#8211;19:20',
+        hours: '6',
+        cost: '380—600 руб',
+        yearsOld: 'Взрослые'
+      }
+    ]
+  }
+};
